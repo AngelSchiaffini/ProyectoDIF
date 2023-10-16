8d73a51e15f6b1267cf8cfe3b7bb39c89b849023
AngelSchiaffini <angelschiaffini@gmail.com>
2023-10-16 03:43:05 -0600

    por si acaso, igual ya todo esta en AWS

--- InicioSesion/codigo.js
+++ InicioSesion/codigo.js
@@ -1,8 +1,23 @@
+function iniciarSesion() {
+    console.log("Hola")
+    const curpUsuario = document.getElementById('curpUsuario').value;
 
-
-function mostrarTutor(){
-
-    tutorForm.style.display = 'block';
-    estudianteForm.style.display = 'none';
+    if (curpUsuario) {
+        // Realiza una solicitud GET al servidor con el valor de 'curpUsuario'
+        fetch(`http://34.197.187.131:8080/inicioSesion/${curpUsuario}`)
+            .then(response => {
+                if (response.status === 200) {
+                    // Redirige a la página "datosUsuario.html" cuando se encuentra el usuario
+                    window.location.href = '/datosUsuario.html';
+                } else {
+                    // Maneja el caso en el que el usuario no se encuentra
+                    alert('Usuario no encontrado');
+                }
+            })
+            .catch(error => {
+                console.error('Error en la solicitud:', error);
+            });
+    } else {
+        alert('Por favor ingresa un valor en el campo Curp / No. Usuario');
+    }5
 }
-
